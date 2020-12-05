5652a8ed43e2bc8c827ca2394c281d01bf55b802
Suman Jha <meetsumanat@gmail.com>
2020-12-05 09:18:10 -0800

    guess a number implementation

---
+++ 20. speak-number/script.js
@@ -0,0 +1,67 @@
+const msgEl = document.getElementById('msg');
+
+const randomNum = getRandomNumber();
+
+console.log('Number: ', randomNum);
+
+window.SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// start recognition and game
+recognition.start();
+
+function onSpeak(e) {
+  const msg = e.results[0][0].transcript;
+  writeMessage(msg);
+  checkNumber(msg);
+}
+
+function writeMessage(msg) {
+  msgEl.innerHTML = `
+    <div> You said: </div>
+    <span class="box">${msg}</span>
+  `;
+}
+
+function checkNumber(msg) {
+  const num = +msg;
+
+  if (Number.isNaN(num)) {
+    msgEl.innerHTML = '<div>That is not a valid number</div>';
+    return;
+  }
+
+  // check range
+  if (num > 100 || num < 1) {
+    msgEl.innerHTML = '<div>Number must be between 1 and 100</div>';
+    return;
+  }
+
+  // check number
+  if (num === randomNum) {
+    document.body.innerHTML = `
+      <h2>Congrats! You have guessed the number! <br><br>
+      It was ${num}</h2>
+      <button class="play-again" id="play-again" onclick="playAgain()">Play Again</button>
+    `;
+  } else if (num > randomNum) {
+    msgEl.innerHTML += `<div>GO LOWER</div>`;
+  } else {
+    msgEl.innerHTML = `<div>GO HIGHER</div>`;
+  }
+}
+
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100) + 1;
+}
+
+function playAgain() {
+  window.location.reload();
+}
+
+// speak result
+recognition.addEventListener('result', onSpeak);
+
+recognition.addEventListener('end', () => recognition.start());
